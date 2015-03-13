cc5f4ee737901a6cade989d4ab92982ca7b54ee2
Jaken Herman <jch053@shsu.edu>
2015-03-13 10:54:26 -0500

    no message

---
+++ btcvt.asm
@@ -0,0 +1,25 @@
+org   0x100                            ;.com files always start 256 bytes into segment
+
+mov   bx, 1                            ;init bx to 1
+mov   ax, 0100h
+
+mov   dx, msg                          ;message's address in dx
+mov   cx, len
+mov   ah, 0x40
+int   0x21
+
+msg db 'Enter a Base Ten Number: ', 0x0d, 0x0a
+len equ $ -msg
+
+mov   ah, 9
+int   21h
+
+while:
+        cmp   ax, 13        ;is char = carriage return?
+        je    endwhile      ;if so, we're done
+        shl   bx, 1         ;multiplies bx by 2
+        and   al, 01h       ;convert character to binary
+        or    bl, al         ;"add" the low bit
+        int   21h
+        jmp   while
+endwhile
