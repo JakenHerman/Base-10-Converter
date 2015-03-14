8591ecb652b02619f5f51ada5be23e2ed11dca13
Jaken Herman <jch053@shsu.edu>
2015-03-14 02:23:41 -0500

    Fixed Auto-cancellation issue. The program now prompts the user for input and allows for user input. Known issue: Does not create line feed, therefore, any time carriage return is selected, the initial user input gets overwritten by any new user input. The program still does not make any actual conversions.

--- btcvt.asm
+++ btcvt.asm
@@ -1,25 +1,34 @@
-org   0x100                            ;.com files always start 256 bytes into segment
+org 100h
+
+
+section .DATA
+prompt   db   "Enter a Base 10 Number",13,10,'$'
+len      equ $ -prompt
+
+
+section .TEXT
+start:
+
+;display prompt
+mov   ah, 09
+mov   dx, prompt
+int   21h
+
 
 mov   bx, 1                            ;init bx to 1
 mov   ax, 0100h
 
-mov   dx, msg                          ;message's address in dx
-mov   cx, len
-mov   ah, 0x40
-int   0x21
 
-msg db 'Enter a Base Ten Number: '
-len equ $ -msg
-
-mov   ah, 9
+mov   ah, 0Ah
 int   21h
 
+
 while:
-        cmp   ax, 13        ;is char = carriage return?
-        je    endwhile      ;if so, we're done
-        shl   bx, 1         ;multiplies bx by 2
-        and   al, 01h       ;convert character to binary
-        or    bl, al         ;"add" the low bit
-        int   21h
-        jmp   while
+    cmp   ax, 13        ;is char = carriage return?
+    je    endwhile      ;if so, we're done
+    shl   bx, 1         ;multiplies bx by 2
+    and   al, 01h       ;convert character to binary
+    or    bl, al        ;"add" the low bit
+    int   21h
+    jmp   while
 endwhile
