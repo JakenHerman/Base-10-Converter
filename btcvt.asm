bda1a013f71e93ee08ca0bf7e0d0674229afb4b7
Jaken Herman <jch053@shsu.edu>
2015-03-19 21:55:35 -0500

    octal now showing, not computer properly though

--- btcvt.asm
+++ btcvt.asm
@@ -10,23 +10,23 @@ section .text
 start:
 	mov	    ah,9		    ; print prompt
 	mov	    dx,prompt1
-	int 	  21h
+	int 	    21h
 
 ; input base 10 value
-	mov   	bx,0	      ; bx holds input value
-	mov	    ah,1		    ; input char function
+	mov   	    bx,0	      ; bx holds input value
+	mov	    ah,1	      ; input char function
 	int	    21h		      ; read char into al
 top1:				          ; while (char != CR)
 	cmp	    al,13		    ; is char = CR?
 	je	    out1		    ; yes?  finished with input
 	and	    ax,000Fh	  ; convert from ASCII to base 10 value
-	push	  ax		      ; and save it on stack
+	push	    ax		      ; and save it on stack
 	mov	    ax,10		    ; set up to multiply bx by 10
 	mul	    bx		      ; dx:ax = bx*10
-	pop   	bx		      ; saved value in bx
+	pop   	    bx		      ; saved value in bx
 	add	    bx,ax		    ; bx = old bx*10 + new digit
 
-  mov	    ah,1		    ; input char function
+        mov	    ah,1		    ; input char function
 	int	    21h		      ; read next character
 	jmp	    top1		    ; loop until done
 
@@ -34,44 +34,44 @@ top1:				          ; while (char != CR)
 out1:
 	mov	    ah,9		    ; print binary output label
 	mov	    dx,prompt2
-	int     21h
+	int         21h
 
 ; for 16 times do this:
 	mov	    cx, 16		  ; loop counter
 
 top2:
-  rol    	bx,1		    ; rotate msb into CF
-	jc    	one		      ; CF = 1?
-	mov    	dl,'0'		  ; no, set up to print a 0
-	jmp	    print		    ; now print
+        rol    	    bx,1              ; rotate most significant bit into carry flag
+	jc    	    one		      ; carry flag = 1?
+	mov    	    dl,'0'	      ; if no, set up to print a 0
+	jmp	    print             ; now print
 one:
-  mov    	dl,'1'		  ; printing a 1
+        mov    	    dl,'1'		  ; printing a 1
 print:
-  mov   	ah,2		    ; print char fcn
-	int	    21h		      ; print it
-	loop	  top2		    ; loop until done
+        mov   	    ah,2		  ; print char fcn
+	int	    21h		          ; print it
+	loop	    top2		  ; loop until done
 
 
 ;output in octal
+
 out2:
    mov    ah, 9       ;print octal output label
    mov    dx,prompt4
    int    21h
 
-; for 12 times do this:
-	mov	    cx, 12		  ; loop counter
+; for 7 times do this:
+	mov	    cx, 3		  ; loop counter
 
-top2:
-  rol    	bx,1		    ; rotate msb into CF
-	jc    	one		      ; CF = 1?
-	mov    	dl,'0'		  ; no, set up to print a 0
-	jmp	    print		    ; now print
-one:
-  mov    	dl,'1'		  ; printing a 1
-print:
-  mov   	ah,2		    ; print char fcn
-	int	    21h		      ; print it
-	loop	  top2		    ; loop until done
+top3:
+        rol	    bx,3		    ; rotate top nybble into the bottom
+	mov	    dl,bl		    ; put a copy in dl
+	and	    dl,0Fh		  ; we only want the lower 4 bits
+	or	    dl,30h		  ; convert 0-9 to '0'-'9'
+	jmp	    print_oc		  ; now print
+print_oc:
+        mov   	ah,2		    ; print char fcn
+	int	21h		      ; print it
+	loop	top3		    ; loop until done
 
 
 ;end octal
@@ -87,8 +87,8 @@ out3:
 ; for 4 times do this:
 	mov    	cx, 4		    ; loop counter
 
-top3:
-  rol	    bx,4		    ; rotate top nybble into the bottom
+top4:
+        rol	    bx,4		    ; rotate top nybble into the bottom
 	mov	    dl,bl		    ; put a copy in dl
 	and	    dl,0Fh		  ; we only want the lower 4 bits
 	cmp	    dl,9		    ; is it in [0-9]?
@@ -100,9 +100,9 @@ AF:
   add   	dl,55		    ; convert 10-15 to 'A'-'F'
 
 print2:
-  mov	    ah,2	    	; print char
+        mov	    ah,2	    	; print char
 	int	    21h
-	loop	  top3	     	; loop until done
+	loop	  top4	     	; loop until done
 
 Exit:
 	mov     ah,04Ch     ;DOS function: Exit program
